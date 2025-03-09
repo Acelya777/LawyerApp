38f8852a5cf765731f8891165e9b0cd67fc112ed
Acelya777 <acelyayildirim2020@gmail.com>
2025-03-10 00:40:31 +0300

    Calender ve Client Activity görünümleri

--- app/src/main/java/com/acelya/lawyerapp/ClientManagement.kt
+++ app/src/main/java/com/acelya/lawyerapp/ClientManagement.kt
@@ -1,20 +1,62 @@
 package com.acelya.lawyerapp
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.acelya.lawyerapp.SignUp.CustomSpinnerAdapter
 
 class ClientManagement : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_client_management)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val rootView = findViewById<View>(android.R.id.content)
+        val clientSpinnerCity = findViewById<Spinner>(R.id.ClientCitySpinner)
+
+        val spinnerOptionsCity = arrayListOf("Seçiniz", "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara",
+            "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt",
+            "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı",
+            "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum",
+            "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır",
+            "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars",
+            "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli",
+            "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+            "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt",
+            "Sinop", "Sivas", "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli",
+            "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak")
+        val adapterCity = CustomSpinnerAdapter(this, android.R.layout.simple_spinner_item, spinnerOptionsCity)
+        adapterCity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        clientSpinnerCity.adapter = adapterCity
+
+        rootView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            private val rect = Rect()
+
+            override fun onGlobalLayout() {
+                rootView.getWindowVisibleDisplayFrame(rect)
+                val screenHeight = rootView.rootView.height
+                val keypadHeight = screenHeight - rect.bottom
+
+                if (keypadHeight > screenHeight * 0.15) { // Klavye açık
+                    setLayoutMargin(1200)
+                } else {
+                    setLayoutMargin(0)
+                }
+            }
+        })
+    }
+    private fun setLayoutMargin(margin: Int) {
+        val loginLayoutT = findViewById<ScrollView>(R.id.ClientManagementView)
+        val layoutParams = loginLayoutT.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = margin
+        loginLayoutT.layoutParams = layoutParams
     }
 }
--- app/src/main/java/com/acelya/lawyerapp/Reminder.kt
+++ app/src/main/java/com/acelya/lawyerapp/Reminder.kt
@@ -1,24 +1,16 @@
 package com.acelya.lawyerapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Calendar : AppCompatActivity() {
+class Reminder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_calendar)
-        val calenderReminder = findViewById<ImageButton>(R.id.CalenderReminderButton)
-
-        calenderReminder.setOnClickListener {
-            val intent = Intent(this,Reminder::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_reminder)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
